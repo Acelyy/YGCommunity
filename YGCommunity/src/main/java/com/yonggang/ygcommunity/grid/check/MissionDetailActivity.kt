9276a11f20940b2a500c270eeb5c871bf7c9050f
Acelyy <1084664238@qq.com>
2018-09-05 14:47:28 +0800

    update

---
+++ YGCommunity/src/main/java/com/yonggang/ygcommunity/grid/check/MissionDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.yonggang.ygcommunity.grid.check
+
+import android.os.Bundle
+import com.yonggang.ygcommunity.BaseActivity
+import com.yonggang.ygcommunity.R
+
+class MissionDetailActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mission_detail)
+    }
+}
