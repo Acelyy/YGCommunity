dda27cf573fed8387d11fb1ced2dff3e109a23ee
Acelyy <1084664238@qq.com>
2018-09-05 14:42:48 +0800

    update

---
+++ YGCommunity/src/main/java/com/yonggang/ygcommunity/Entry/MissionBean.java
@@ -0,0 +1,58 @@
+package com.yonggang.ygcommunity.Entry;
+
+public class MissionBean {
+
+    /**
+     * id : 4
+     * sbsj : 2018-09-05
+     * sjbt : test1
+     * sjms :
+     * status : 4
+     */
+
+    private String id;
+    private String sbsj;
+    private String sjbt;
+    private String sjms;
+    private int status;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSbsj() {
+        return sbsj;
+    }
+
+    public void setSbsj(String sbsj) {
+        this.sbsj = sbsj;
+    }
+
+    public String getSjbt() {
+        return sjbt;
+    }
+
+    public void setSjbt(String sjbt) {
+        this.sjbt = sjbt;
+    }
+
+    public String getSjms() {
+        return sjms;
+    }
+
+    public void setSjms(String sjms) {
+        this.sjms = sjms;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
