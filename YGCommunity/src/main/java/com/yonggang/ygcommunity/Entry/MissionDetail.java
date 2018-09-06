78c00a3931f150ea307e4717a2c2981c9181b14f
Acelyy <1084664238@qq.com>
2018-09-06 10:06:18 +0800

    update

---
+++ YGCommunity/src/main/java/com/yonggang/ygcommunity/Entry/MissionDetail.java
@@ -0,0 +1,110 @@
+package com.yonggang.ygcommunity.Entry;
+
+import java.util.List;
+
+public class MissionDetail {
+
+    /**
+     * czfa : 上报
+     * girdimgs : ["http://zhyl.yong-gang.com/gird_img/20180906/imgs201809061588356869.jpg"]
+     * sjbh : APP20180906092610
+     * sjbt : 好多女的不对不对
+     * sjdw : 江苏省苏州市张家港市永琪路靠近永联小镇
+     * sjfl : 电动自行车停放充电
+     * sjms : nxnxnxxnndndnddnddnndndndndnxbnxnxbxbxbxbxbxbxbxbxbxbxbx不行不行你喜欢的花生牛奶苏宁对你的难道你男的女的你打不打八点半到八点bxbbxnx
+     * sswg : 1
+     * status : 2
+     * yzcd : 普通
+     */
+
+    private String czfa;
+    private String sjbh;
+    private String sjbt;
+    private String sjdw;
+    private String sjfl;
+    private String sjms;
+    private String sswg;
+    private int status;
+    private String yzcd;
+    private List<String> girdimgs;
+
+    public String getCzfa() {
+        return czfa;
+    }
+
+    public void setCzfa(String czfa) {
+        this.czfa = czfa;
+    }
+
+    public String getSjbh() {
+        return sjbh;
+    }
+
+    public void setSjbh(String sjbh) {
+        this.sjbh = sjbh;
+    }
+
+    public String getSjbt() {
+        return sjbt;
+    }
+
+    public void setSjbt(String sjbt) {
+        this.sjbt = sjbt;
+    }
+
+    public String getSjdw() {
+        return sjdw;
+    }
+
+    public void setSjdw(String sjdw) {
+        this.sjdw = sjdw;
+    }
+
+    public String getSjfl() {
+        return sjfl;
+    }
+
+    public void setSjfl(String sjfl) {
+        this.sjfl = sjfl;
+    }
+
+    public String getSjms() {
+        return sjms;
+    }
+
+    public void setSjms(String sjms) {
+        this.sjms = sjms;
+    }
+
+    public String getSswg() {
+        return sswg;
+    }
+
+    public void setSswg(String sswg) {
+        this.sswg = sswg;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getYzcd() {
+        return yzcd;
+    }
+
+    public void setYzcd(String yzcd) {
+        this.yzcd = yzcd;
+    }
+
+    public List<String> getGirdimgs() {
+        return girdimgs;
+    }
+
+    public void setGirdimgs(List<String> girdimgs) {
+        this.girdimgs = girdimgs;
+    }
+}
