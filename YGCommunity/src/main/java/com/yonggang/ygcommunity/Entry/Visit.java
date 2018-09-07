32092d6071ca6966ff87cb67cbe62d9a82a3e83e
H-494 <614711977@qq.com>
2018-09-07 10:18:10 +0800

    ...

---
+++ YGCommunity/src/main/java/com/yonggang/ygcommunity/Entry/Visit.java
@@ -0,0 +1,68 @@
+package com.yonggang.ygcommunity.Entry;
+
+public class Visit {
+
+    /**
+     * id : 11
+     * pcsj : 2018-09-07
+     * swqk : 哈哈蛤
+     * telephone : 666
+     * xwqk : 哈哈蛤
+     * zdry : 哈哈蛤
+     */
+
+    private String id;
+    private String pcsj;
+    private String swqk;
+    private String telephone;
+    private String xwqk;
+    private String zdry;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPcsj() {
+        return pcsj;
+    }
+
+    public void setPcsj(String pcsj) {
+        this.pcsj = pcsj;
+    }
+
+    public String getSwqk() {
+        return swqk;
+    }
+
+    public void setSwqk(String swqk) {
+        this.swqk = swqk;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getXwqk() {
+        return xwqk;
+    }
+
+    public void setXwqk(String xwqk) {
+        this.xwqk = xwqk;
+    }
+
+    public String getZdry() {
+        return zdry;
+    }
+
+    public void setZdry(String zdry) {
+        this.zdry = zdry;
+    }
+}
