5f369c2fa5b56de5071ed6727c8cf5fb7ee580d3
Acelyy <1084664238@qq.com>
2018-09-12 15:24:00 +0800

    update

---
+++ YGCommunity/src/main/java/com/yonggang/ygcommunity/Entry/Trail.java
@@ -0,0 +1,58 @@
+package com.yonggang.ygcommunity.Entry;
+
+public class Trail {
+
+    /**
+     * bm : 惠邻社工
+     * bpname : 陶莉莉
+     * comment : 甩锅
+     * status : 7
+     * time : 2018-09-12
+     */
+
+    private String bm;
+    private String bpname;
+    private String comment;
+    private int status;
+    private String time;
+
+    public String getBm() {
+        return bm;
+    }
+
+    public void setBm(String bm) {
+        this.bm = bm;
+    }
+
+    public String getBpname() {
+        return bpname;
+    }
+
+    public void setBpname(String bpname) {
+        this.bpname = bpname;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
