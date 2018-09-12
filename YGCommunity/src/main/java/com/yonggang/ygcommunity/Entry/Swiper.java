86ed18d90ef5a721e568e21c09c3b10a6549dba2
H-494 <614711977@qq.com>
2018-09-12 09:07:21 +0800

    ...

---
+++ YGCommunity/src/main/java/com/yonggang/ygcommunity/Entry/Swiper.java
@@ -0,0 +1,27 @@
+package com.yonggang.ygcommunity.Entry;
+
+public class Swiper {
+    /**
+     * imgurl : zhyl.yong-gang.com/dzyl/20180912/1.png
+     * status : 1
+     */
+
+    private String imgurl;
+    private String status;
+
+    public String getImgurl() {
+        return imgurl;
+    }
+
+    public void setImgurl(String imgurl) {
+        this.imgurl = imgurl;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
