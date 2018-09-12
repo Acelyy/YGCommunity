5a550515c3e9f907db21ba288a932f5958ceb67c
H-494 <614711977@qq.com>
2018-09-12 16:03:16 +0800

    ...

--- YGCommunity/src/main/java/com/yonggang/ygcommunity/grid/mission/TrailActivity.kt
+++ YGCommunity/src/main/java/com/yonggang/ygcommunity/grid/mission/TrailActivity.kt
@@ -1,22 +1,29 @@
 package com.yonggang.ygcommunity.grid.mission
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
+import com.yonggang.ygcommunity.BaseActivity
 import com.yonggang.ygcommunity.Entry.Trail
 import com.yonggang.ygcommunity.R
+import com.yonggang.ygcommunity.Util.StatusBarUtil
 import com.yonggang.ygcommunity.httpUtil.HttpUtil
-import kotlinx.android.synthetic.main.activity_transfer.*
+import kotlinx.android.synthetic.main.activity_trail.*
 import rx.Subscriber
 
-class TrailActivity : AppCompatActivity() {
+class TrailActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trail)
+        StatusBarUtil.setColor(this, resources.getColor(R.color.refresh_color), 0)
         val id = intent.getStringExtra("id")
         refresh.setOnRefreshListener {
             getTrail(id)
@@ -31,6 +38,7 @@ class TrailActivity : AppCompatActivity() {
         val subscriber = object : Subscriber<List<Trail>>() {
             override fun onNext(t: List<Trail>?) {
                 refresh.finishRefresh()
+                list_trail.adapter = TrailAdapter(t!!, this@TrailActivity)
             }
 
             override fun onCompleted() {
@@ -46,23 +54,65 @@ class TrailActivity : AppCompatActivity() {
     }
 
 
-    class TrailAdapter():BaseAdapter(){
+    class TrailAdapter(val data: List<Trail>, val context: Context) : BaseAdapter() {
         override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            val view: View
+            val mholder: ViewHolder
+            if (convertView == null) {
+                view = LayoutInflater.from(context).inflate(R.layout.item_trail_layout, parent, false)
+                mholder = ViewHolder(view)
+                view.tag = mholder
+            } else {
+                view = convertView
+                mholder = view.tag as ViewHolder
+            }
+            var s = ""
+            if (data[position].bm != null) {
+                s += "部门：" + data[position].bm + "\n"
+            }
+            if (data[position].bpname != null) {
+                s += "处理人：" + data[position].bpname + "\n"
+            }
+            if (data[position].comment != null) {
+                s += "处理意见：" + data[position].comment + "\n"
+            }
+            s += "处理状态：" + when (data[position].status) {
+                0 -> "草稿"
+                1 -> "待签收"
+                2 -> "已签收"
+                3 -> "平台自行处理"
+                4 -> "核查通知"
+                5 -> "转派部门"
+                6 -> "部门签收"
+                7 -> "部门处理"
+                8 -> "部门完结"
+                9 -> "任务完成"
+                else -> {
+                    ""
+                }
+            } + "\n"
+            if (data[position].time != null) {
+                s += "处理时间：" + data[position].time
+            }
+            mholder.textinfo.text = s
+            return view
         }
 
         override fun getItem(position: Int): Any {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            return data[position]
         }
 
         override fun getItemId(position: Int): Long {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            return position.toLong()
         }
 
         override fun getCount(): Int {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            return data.size
         }
 
+        inner class ViewHolder(view: View) {
+            val textinfo = view.findViewById(R.id.info) as TextView
+        }
     }
 
 }
