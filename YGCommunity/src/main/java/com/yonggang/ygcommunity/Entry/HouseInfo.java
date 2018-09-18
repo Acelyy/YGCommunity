c69e17f725a0966631442d7e4444880889df96bb
H-494 <614711977@qq.com>
2018-09-18 09:23:03 +0800

    ...

---
+++ YGCommunity/src/main/java/com/yonggang/ygcommunity/Entry/HouseInfo.java
@@ -0,0 +1,394 @@
+package com.yonggang.ygcommunity.Entry;
+
+public class HouseInfo {
+
+    /**
+     * bz :
+     * comment :
+     * cph : 1
+     * csrq : 1992-12-13
+     * fdlxdh : 1
+     * fwbm_pk : d362bfc4-ab67-96ad-9533-ef075ceee0d4
+     * gzdw : 阿里妈妈
+     * hjbh :
+     * hjdz : 江苏省江阴市华士镇华西一村塔家基51号
+     * hyzk : 初婚
+     * lxdh : 666666666
+     * mz : 汉族
+     * rypk : bff1f097-86e6-112f-13d5-e8ee8b7a2280
+     * sfcj : 0
+     * sfdbh : 0
+     * sfdj : 0
+     * sffd : 0
+     * sfhj :
+     * sfjsb : 0
+     * sfkc : 0
+     * sflkbk : 0
+     * sfpk : 0
+     * sfsy :
+     * sftf : 0
+     * sfxsm : 0
+     * sfyf : 0
+     * sfzb : 0
+     * sfzh : 320281199212133310
+     * type : 流动人口
+     * whcd : 硕士及以上
+     * xb : 男
+     * xm : 李阳洋
+     * xqah :
+     * zzmm : 团员
+     */
+
+    private String bz;
+    private String comment;
+    private String cph;
+    private String csrq;
+    private String fdlxdh;
+    private String fwbm_pk;
+    private String gzdw;
+    private String hjbh;
+    private String hjdz;
+    private String hyzk;
+    private String lxdh;
+    private String mz;
+    private String rypk;
+    private int sfcj;
+    private int cjdj;
+    private int sfdbh;
+    private int sfdj;
+    private int sffd;
+    private int sfhj;
+    private int sfjsb;
+    private int sfkc;
+    private int sflkbk;
+    private int sfpk;
+    private int sfsy;
+    private int sftf;
+    private int sfxsm;
+    private int sfyf;
+    private int sfzb;
+    private String sfzh;
+    private String type;
+    private String whcd;
+    private String xb;
+    private String xm;
+    private String xqah;
+    private String zzmm;
+    private String zdxx;
+    private String xjzdz;
+    private String zyzzh;
+    private String tplj;
+
+    public String getZyzzh() {
+        return zyzzh;
+    }
+
+    public void setZyzzh(String zyzzh) {
+        this.zyzzh = zyzzh;
+    }
+
+    public String getTplj() {
+        return tplj;
+    }
+
+    public void setTplj(String tplj) {
+        this.tplj = tplj;
+    }
+
+    public String getBz() {
+        return bz;
+    }
+
+    public void setBz(String bz) {
+        this.bz = bz;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getCph() {
+        return cph;
+    }
+
+    public void setCph(String cph) {
+        this.cph = cph;
+    }
+
+    public String getCsrq() {
+        return csrq;
+    }
+
+    public void setCsrq(String csrq) {
+        this.csrq = csrq;
+    }
+
+    public String getFdlxdh() {
+        return fdlxdh;
+    }
+
+    public void setFdlxdh(String fdlxdh) {
+        this.fdlxdh = fdlxdh;
+    }
+
+    public String getFwbm_pk() {
+        return fwbm_pk;
+    }
+
+    public void setFwbm_pk(String fwbm_pk) {
+        this.fwbm_pk = fwbm_pk;
+    }
+
+    public String getGzdw() {
+        return gzdw;
+    }
+
+    public void setGzdw(String gzdw) {
+        this.gzdw = gzdw;
+    }
+
+    public String getHjbh() {
+        return hjbh;
+    }
+
+    public void setHjbh(String hjbh) {
+        this.hjbh = hjbh;
+    }
+
+    public String getHjdz() {
+        return hjdz;
+    }
+
+    public void setHjdz(String hjdz) {
+        this.hjdz = hjdz;
+    }
+
+    public String getHyzk() {
+        return hyzk;
+    }
+
+    public void setHyzk(String hyzk) {
+        this.hyzk = hyzk;
+    }
+
+    public String getLxdh() {
+        return lxdh;
+    }
+
+    public void setLxdh(String lxdh) {
+        this.lxdh = lxdh;
+    }
+
+    public String getMz() {
+        return mz;
+    }
+
+    public void setMz(String mz) {
+        this.mz = mz;
+    }
+
+    public String getRypk() {
+        return rypk;
+    }
+
+    public void setRypk(String rypk) {
+        this.rypk = rypk;
+    }
+
+    public int getSfcj() {
+        return sfcj;
+    }
+
+    public void setSfcj(int sfcj) {
+        this.sfcj = sfcj;
+    }
+
+    public int getSfdbh() {
+        return sfdbh;
+    }
+
+    public void setSfdbh(int sfdbh) {
+        this.sfdbh = sfdbh;
+    }
+
+    public int getSfdj() {
+        return sfdj;
+    }
+
+    public void setSfdj(int sfdj) {
+        this.sfdj = sfdj;
+    }
+
+    public int getSffd() {
+        return sffd;
+    }
+
+    public void setSffd(int sffd) {
+        this.sffd = sffd;
+    }
+
+    public int getSfhj() {
+        return sfhj;
+    }
+
+    public void setSfhj(int sfhj) {
+        this.sfhj = sfhj;
+    }
+
+    public int getSfjsb() {
+        return sfjsb;
+    }
+
+    public void setSfjsb(int sfjsb) {
+        this.sfjsb = sfjsb;
+    }
+
+    public int getSfkc() {
+        return sfkc;
+    }
+
+    public void setSfkc(int sfkc) {
+        this.sfkc = sfkc;
+    }
+
+    public int getSflkbk() {
+        return sflkbk;
+    }
+
+    public void setSflkbk(int sflkbk) {
+        this.sflkbk = sflkbk;
+    }
+
+    public int getSfpk() {
+        return sfpk;
+    }
+
+    public void setSfpk(int sfpk) {
+        this.sfpk = sfpk;
+    }
+
+    public int getSfsy() {
+        return sfsy;
+    }
+
+    public void setSfsy(int sfsy) {
+        this.sfsy = sfsy;
+    }
+
+    public int getSftf() {
+        return sftf;
+    }
+
+    public void setSftf(int sftf) {
+        this.sftf = sftf;
+    }
+
+    public int getSfxsm() {
+        return sfxsm;
+    }
+
+    public void setSfxsm(int sfxsm) {
+        this.sfxsm = sfxsm;
+    }
+
+    public int getSfyf() {
+        return sfyf;
+    }
+
+    public void setSfyf(int sfyf) {
+        this.sfyf = sfyf;
+    }
+
+    public int getSfzb() {
+        return sfzb;
+    }
+
+    public void setSfzb(int sfzb) {
+        this.sfzb = sfzb;
+    }
+
+    public String getSfzh() {
+        return sfzh;
+    }
+
+    public void setSfzh(String sfzh) {
+        this.sfzh = sfzh;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getWhcd() {
+        return whcd;
+    }
+
+    public void setWhcd(String whcd) {
+        this.whcd = whcd;
+    }
+
+    public String getXb() {
+        return xb;
+    }
+
+    public void setXb(String xb) {
+        this.xb = xb;
+    }
+
+    public String getXm() {
+        return xm;
+    }
+
+    public void setXm(String xm) {
+        this.xm = xm;
+    }
+
+    public String getXqah() {
+        return xqah;
+    }
+
+    public void setXqah(String xqah) {
+        this.xqah = xqah;
+    }
+
+    public String getZzmm() {
+        return zzmm;
+    }
+
+    public void setZzmm(String zzmm) {
+        this.zzmm = zzmm;
+    }
+
+    public String getZdxx() {
+        return zdxx;
+    }
+
+    public void setZdxx(String zdxx) {
+        this.zdxx = zdxx;
+    }
+
+    public String getXjzdz() {
+        return xjzdz;
+    }
+
+    public void setXjzdz(String xjzdz) {
+        this.xjzdz = xjzdz;
+    }
+
+
+    public int getCjdj() {
+        return cjdj;
+    }
+
+    public void setCjdj(int cjdj) {
+        this.cjdj = cjdj;
+    }
+}
