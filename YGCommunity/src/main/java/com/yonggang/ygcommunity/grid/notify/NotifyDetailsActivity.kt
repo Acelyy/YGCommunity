fd342d7e0d7f22e82f9e6f2ba60da4e177f4d946
494 <614711977@qq.com>
2018-11-12 09:59:30 +0800

    。。。

--- YGCommunity/src/main/java/com/yonggang/ygcommunity/grid/notify/NotifyDetailsActivity.kt
+++ YGCommunity/src/main/java/com/yonggang/ygcommunity/grid/notify/NotifyDetailsActivity.kt
@@ -1,16 +1,13 @@
 package com.yonggang.ygcommunity.grid.notify
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.umeng.socialize.utils.Log
+import android.view.KeyEvent
+import com.tencent.smtt.sdk.*
 import com.yonggang.ygcommunity.BaseActivity
 import com.yonggang.ygcommunity.R
 import com.yonggang.ygcommunity.Util.StatusBarUtil
 import com.yonggang.ygcommunity.httpUtil.HttpUtil
-import com.yonggang.ygcommunity.httpUtil.ProgressSubscriber
-import com.yonggang.ygcommunity.httpUtil.SubscriberOnNextListener
 import kotlinx.android.synthetic.main.activity_notice_detail.*
-import rx.Subscriber
 
 class NotifyDetailsActivity : BaseActivity() {
 
@@ -21,10 +18,29 @@ class NotifyDetailsActivity : BaseActivity() {
         StatusBarUtil.setColor(this, resources.getColor(R.color.refresh_color), 0)
         id = intent.getStringExtra("id")
         val string = HttpUtil.BASE_URL + "gird_get_jlist_detail/id/" + id
-        if (id != ""){
+        if (id != "") {
             webView.loadUrl(string)
+            webView.setWebViewClient(YgWebViewClient())
         }
     }
 
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (webView.canGoBack()) {
+                webView.goBack()
+            } else {
+                finish()
+            }
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
 
+    // Web视图
+    private inner class YgWebViewClient : WebViewClient() {
+        override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+            view!!.loadUrl(url)
+            return true
+        }
+    }
 }
